316af88727fb1665436c079ca7f6b40fb76ab1e9
CrepuscularSoul <damon.drozd@gmail.com>
2019-07-03 09:27:44 -0400

    Changed a bunch of things to use constants and added Norvrandt weather (#8)

---
+++ WeatherApp.Domain/Models/Constants.cs
@@ -0,0 +1,95 @@
+﻿namespace WeatherApp.Domain.Models
+{
+    public class Constants
+    {
+        public static class Weather
+        {
+            public const string
+                Blizzards = "Blizzards",
+                ClearSkies = "Clear Skies",
+                Clouds = "Clouds",
+                DustStorms = "Dust Storms",
+                FairSkies = "Fair Skies",
+                Fog = "Fog",
+                Gales = "Gales",
+                Gloom = "Gloom",
+                HeatWaves = "Heat Waves",
+                Rain = "Rain",
+                Showers = "Showers",
+                Snow = "Snow",
+                Thunder = "Thunder",
+                Thunderstorms = "Thunderstorms",
+                UmbralStatic = "Umbral Static",
+                UmbralWind = "Umbral Wind",
+                Wind = "Wind";
+        }
+
+        public static class Zones
+        {
+            public const string
+                AmhAraeng = "Amh Araeng",
+                AzimSteppe = "The Azim Steppe",
+                AzysLla = "Azys Lla",
+                CentralShroud = "Central Shroud",
+                CentralThanalan = "Central Thanalan",
+                ChurningMists = "The Churning Mists",
+                CoerthasCentralHighlands = "Coerthas Central Highlands",
+                CoerthasWesternHighlands = "Coerthas Western Highlands",
+                Crystarium = "The Crystarium",
+                DravanianForelands = "The Dravanian Forelands",
+                DravanianHinterlands = "The Dravanian Hinterlands",
+                EasternLaNoscea = "Eastern La Noscea",
+                EasternThanalan = "Eastern Thanalan",
+                EastShroud = "East Shroud",
+                Eulmore = "Eulmore",
+                EurekaAnemos = "Eureka Anemos",
+                EurekaHydatos = "Eureka Hydatos",
+                EurekaPagos = "Eureka Pagos",
+                EurekaPyros = "Eureka Pyros",
+                Fringes = "The Fringes",
+                Goblet = "The Goblet",
+                Gridania = "Gridania",
+                Idyllshire = "Idyllshire",
+                IlMheg = "Il Mheg",
+                Ishgard = "Ishgard",
+                Kholusia = "Kholusia",
+                Kugane = "Kugane",
+                Lakeland = "Lakeland",
+                LavenderBeds = "The Lavender Beds",
+                LimsaLominsa = "Limsa Lominsa",
+                Lochs = "The Lochs",
+                LowerLaNoscea = "Lower La Noscea",
+                MiddleLaNoscea = "Middle La Noscea",
+                Mist = "Mist",
+                MorDhona = "Mor Dhona",
+                NorthernThanalan = "Northern Thanalan",
+                NorthShroud = "North Shroud",
+                OuterLaNoscea = "Outer La Noscea",
+                Peaks = "The Peaks",
+                RaktikaGreatwood = "The Rak'tika Greatwood",
+                RhalgrsReach = "Rhalgr's Reach",
+                RubySea = "The Ruby Sea",
+                SeaOfClouds = "The Sea of Clouds",
+                SouthernThanalan = "Southern Thanalan",
+                SouthShroud = "South Shroud",
+                Uldah = "Ul'dah",
+                UpperLaNoscea = "Upper La Noscea",
+                WesternLaNoscea = "Western La Noscea",
+                WesternThanalan = "Western Thanalan",
+                Yanxia = "Yanxia";
+        }
+
+        public class Regions
+        {
+            public const string
+                BlackShroud = "Black Shroud",
+                LaNoscea = "La Noscea",
+                Thanalan = "Thanalan",
+                Ishgard = "Ishgard/Surrounding",
+                GyrAbania = "Gyr Abania",
+                FarEast = "Far East",
+                Norvrandt = "Norvrandt",
+                Others = "Others";
+        }
+    }
+}
